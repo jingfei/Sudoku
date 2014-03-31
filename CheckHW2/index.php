eee5541fb340d447026ea3c70aafe1d317731905
JingFei Yang <jingfei@2014cpp.(none)>
2014-03-31 15:54:23 +0800

    不同步Checker & Tournament

--- CheckHW2/index.php
+++ CheckHW2/index.php
@@ -9,6 +9,30 @@
 		<link rel="stylesheet" href="style/style.css"/>
 		<link rel="stylesheet" href="style/form.css"/>
 		<script type="text/javascript" src="script/jquery-2.0.3.js"></script>
+		<style>
+#SudokuImage{
+	position: absolute;
+	bottom: 70px;
+	display: block;
+	right: 30px;
+	background: transparent;
+	width: 80%;
+}
+#SudokuImage:hover{
+	-webkit-border-radius: 10px;
+	-moz-border-radius: 10px;
+	border-radius: 10px;
+	border: none;
+	/* Fallback for web browsers that don't support RGBa */
+    background-color: rgb(0, 0, 0);
+    /* RGBa with 0.6 opacity */
+    background-color: rgba(0, 0, 0, 0.2);
+    /* For IE 5.5 - 7*/
+    filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);
+    /* For IE 8*/
+    -ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)";
+}
+</style>
 	</head>
 		<script>
 function Login(){
@@ -30,7 +54,7 @@ function Login(){
 	return false;
 }
 		</script>
-	<body>
+	<body style="width:100%">
 		<h1 align="center" style="padding:20px;">Please login with your student ID and the password used in moodle</h1>
 		<form class="form-1" onsubmit="return Login();"> 
 		    <p class="field">
@@ -47,6 +71,12 @@ function Login(){
 		</form>
 		<div id="Waiting" style="text-align:center;margin:60px;display:none;">
 			<img src="ajax-loader.gif"/>
+		</div>
+
+		<div style="width:80%;text-align:center;">
+			<div  id="SudokuImage">
+			<a href="http://judge.imslab.org/Tournament"><img src="../image/logo.png"/></a>		
+			</div>
 		</div>
 		<?php include "footer.php"; ?>
 	</body>
